d988c52d140236584f3af21e7ecd1cba70fabe53
Thanakorn Ariyagusolsuthi <thanakorn.ari@truedigital.com>
2022-01-24 15:16:01 +0700

    feat: add auth middleware

--- main.go
+++ main.go
@@ -2,28 +2,34 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 
 	db "github.com/thnkrn/go-gin-postgres/config"
+	auth "github.com/thnkrn/go-gin-postgres/middleware"
 	routers "github.com/thnkrn/go-gin-postgres/routers"
 )
 
 func main() {
 	router := gin.New()
 
+	// Status Response
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "HELLO GOLANG RESTFUL API.",
+			"message": "HELLO WORLD",
 		})
 	})
 
+	// Request JWT
+	router.POST("/login", auth.LoginHandler)
+
 	database := db.Connect()
 
-	api := router.Group("/api")
+	// Authorization
+	api := router.Group("/api", auth.AuthorizationMiddleware)
 	routers.SetCollectionRoutes(api, database)
 
 	fmt.Println("Server Running on Port: ", 3000)
-	http.ListenAndServe(":3000", router)
+	// http.ListenAndServe(":3000", router)
+	router.Run(":3000")
 }
