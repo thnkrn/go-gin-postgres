71111484c527b6f5bd1b38b12f5f334e5316696f
Thanakorn Ariyagusolsuthi <thanakorn.ari@truedigital.com>
2022-01-28 10:36:05 +0700

    fix: adjust dto

--- dto/dto.go
+++ dto/dto.go
@@ -1,12 +1,12 @@
 package dto
 
-// import "time"
+import "time"
 
 type CampaignDTO struct {
 	ID           uint   `json:"id,string,omitempty"`
 	CampaignName string `json:"campaignName" binding:"required"`
 	Date         string `json:"date" binding:"required"`
 	// NOTE: Optional
-	// CreatedAt    time.Time
-	// UpdatedAt    time.Time
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
